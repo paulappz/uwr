d344619dbbd4f6dd6ff57668a277075107637059
paulappz <you@example.com>
2017-02-02 16:16:32 +0100

    update uwr

--- db_con.php
+++ db_con.php
@@ -1,20 +1,22 @@
 <?php
 /* Database config  */
-
+/* 
 $db_host		= getenv('DB_HOSTNAME_UWR');
 $db_user		= getenv('DB_USER_UWR');
 $db_pass		= getenv('DB_PASS_UWR');
 $db_database	= getenv('DB_SCHEMA_UWR'); 
 
+
+Database=;Data Source=;User Id=;Password=
 // End config  
 
-/* 
-
-$db_host		= "localhost";
-$db_user		= "root";
-$db_pass		= "";
-$db_database	= "uwr";
 */
+
+$db_host		= "us-cdbr-azure-southcentral-f.cloudapp.net";
+$db_user		= "b90cc7dd1debdf";
+$db_pass		= "a049a30e";
+$db_database	= "uwrdb";
+
 
 $con = mysqli_connect($db_host,$db_user,$db_pass,$db_database);
 
